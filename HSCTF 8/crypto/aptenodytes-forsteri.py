8648f14caae1fb297d5c6f72af8e8e65d2564cfc
woanmeo11 <woanmeo11@gmail.com>
2021-11-30 15:54:55 +0700

    just update

--- HSCTF 8/crypto/aptenodytes-forsteri.py
+++
@@ -1,8 +0,0 @@
-# HSCTF 8
-# crypto: aptenodytes-forsteri
-# woanmeo11
-
-letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-encoded = "IOWJLQMAGH"
-for c in encoded:
-    print(letters[(letters.index(c) - 18)%26], end='')
